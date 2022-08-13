565e29dd822e8dc5c171f022b82dde9c58140d02
Miguel Gutierrez <miguelepst@hotmail.com>
2022-08-13 02:26:58 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'acumulador-angular Cartagena Bolivar';
+  title = 'Acumulador-angular Cartagena Bolivar';
 }
